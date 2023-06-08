9e50e5ceebc5ce7982b9ffac7ed3c85048efc540
Nikhil <nikhilshah.net@hotmail.com>
2023-06-08 10:23:13 +0530

    Initial Commit

---
+++ axa_skilling_2_vs/axa_skilling_2_vs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace axa_skilling_2_vs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
